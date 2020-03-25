e408ede43eddea968aed4d8854204811851a8a5a
C. S. F. Junior <csf.junior90@gmail.com>
2020-03-25 15:33:14 -0300

    Update mime_types.py

--- mime_types.py
+++ mime_types.py
@@ -9,14 +9,14 @@ q = int(input())  # Number Q of file names to be analyzed.
 d = {ext: mt for ext, mt in [input().split() for _ in range(n)]} # d: dict for association table, ext: file extension, mt: MIME type.
 fname = [input().lower() for _ in range(q)] # One file name per line.
 
-for f in fname: # Percorre lista de arquivos
-    if '.' in f: # Verifica se arquivo tem extensão
-        x = f.split('.')[-1] # Separa extensão
-        if x in d: # Verifica se extensão está na tabela
-            print(d[x]) # Retorna MIME Type
+for f in fname: # Go through files list
+    if '.' in f: # Checks if file has extension
+        x = f.split('.')[-1] # Split extension
+        if x in d: # Checks if extension is in table
+            print(d[x]) # Returns MIME Type
         elif x.upper() in d:
             print(d[x.upper()])
         else:
-            print('UNKNOWN') # Caso não esteja na tabela
+            print('UNKNOWN') # If extension isn't in table
     else:
-        print('UNKNOWN') # Caso não tenha extensão
+        print('UNKNOWN') # If file doesn't have extension
